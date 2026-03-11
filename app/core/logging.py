5e04fa78cf93ada6b57ce23f66bb146aa422f429
Kaung Hein Htet <kaungheinhtetutycc@gmail.com>
2026-03-11 10:24:15 +0700

    docs: add comments

--- app/core/logging.py
+++ app/core/logging.py
@@ -8,10 +8,15 @@ def setup_logging() -> None:
     log_level = getattr(logging, settings.LOG_LEVEL.upper(), logging.INFO)
 
     logging.basicConfig(
+        # set log level
         level=log_level,
+        # set format
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        # print out to terminal
         handlers=[logging.StreamHandler(sys.stdout)],
+        # overwrite any existing logggin setup
         force=True,
     )
 
+    # get the logger named app and set the log level
     logging.getLogger("app").setLevel(log_level)
