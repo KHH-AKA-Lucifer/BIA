010d3e6c18fa0cd137123e3fafe006aa0ceb0b8b
Kaung Hein Htet <kaungheinhtetutycc@gmail.com>
2026-03-11 10:02:29 +0700

    docs: add comments

--- app/core/config.py
+++ app/core/config.py
@@ -2,30 +2,37 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict 
 
 class Settings(BaseSettings):
+    """
+    Settings class thar inheritent the BaseSettings class.
+    """
+
+    # initialize variable, variable type and assign default values 
+    # basic info
     APP_NAME: str = "Data Dashboard Backend"
     APP_ENV: str = "development"
     DEBUG: bool = True
-
     API_V1_STR: str = "/api/v1"
-
+    # postgreSQL
     POSTGRES_HOST: str = "localhost"
     POSTGRES_PORT: int = 5432
     POSTGRES_DB: str = "datadashboard"
-    POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "postgres"
-
-    JWT_SECRET_KEY: str = "change_this_to_a_long_random_secret"
+    POSTGRES_USER: str 
+    POSTGRES_PASSWORD: str 
+    # JWT 
+    JWT_SECRET_KEY: str 
     JWT_ALGORITHM: str = "HS256"
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-
+    # LOGs
     LOG_LEVEL: str = "INFO"
 
+    # configuration for the secrets 
     model_config = SettingsConfigDict(
         env_file = ".env",
         env_file_encoding="utf-8",
         case_sensitive=True
     )
 
+    # decortor that turns method into a variable
     @property
     def database_url(self) -> str:
         return(
