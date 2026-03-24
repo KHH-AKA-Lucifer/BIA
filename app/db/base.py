7b6ec44363daa961f4d9463a707d00225a72a78b
Kaung Hein Htet <kaungheinhtetutycc@gmail.com>
2026-03-24 11:59:02 +0700

    feat: change directory

--- app/db/base.py
+++
@@ -1,6 +0,0 @@
-# import required libraries
-from sqlalchemy.orm import DeclarativeBase
-# create base Registry 
-# to keep track of every table that we define in our entire app
-class Base(DeclarativeBase):
-    pass 
